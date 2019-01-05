c7f1a903d6550a2ff6e0e9e19ab29e4745ad863a
EduarteCliff <37478960+EduarteCliff@users.noreply.github.com>
2019-01-05 20:02:46 +0800

    Create class.sql.php

---
+++ class/class.sql.php
@@ -0,0 +1,25 @@
+<?php
+	public class sql{
+		public $location = "127.0.0.1";
+		public $usr;
+		public $pwd;
+		public $db;
+		
+		function query($query){
+			$connect = mysqli_connect($this->location,$this->usr,$this->pwd);
+			mysqli_select_db($connect,$this->db);
+			$result = mysqli_query($connect,$query);
+			mysql_close($connect);
+			decode($result);
+		}
+		
+		function decode($result){
+			$tag = 0;
+			while($row = mysqli_fetch_row($result)){
+				$return[$tag] = $row[0];
+				$tag++;
+			}
+			return $return;
+		}
+	}
+?>
