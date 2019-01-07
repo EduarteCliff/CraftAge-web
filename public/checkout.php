4bd881e7efdc1ab6b18890e9c780c0257f52158b
EduarteCliff <37478960+EduarteCliff@users.noreply.github.com>
2019-01-07 10:32:11 +0800

    Update checkout.php

--- public/checkout.php
+++ public/checkout.php
@@ -1,4 +1,12 @@
 <?php
+  include "../functions/is_logged_in.php";
+	include "../class/class.sql.php";
+	include "../config/config.php";
+  $sql = new sql();
+	$sql->usr = DB_USR;
+	$sql->pwd = DB_PWD;
+	$sql->db = DB_NAME;
+  $sql->query("");
   echo "the checkout page";
   echo "<br>";
   session_start();
