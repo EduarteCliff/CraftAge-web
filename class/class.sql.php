9d31d6985a6ec41e59009c3d34ae3b205d8eda9f
EduarteCliff <37478960+EduarteCliff@users.noreply.github.com>
2019-01-12 23:14:02 +0800

    Update class.sql.php

--- class/class.sql.php
+++ class/class.sql.php
@@ -5,23 +5,23 @@
 		public $pwd;
 		public $db;
 		
-      	        //调用方式 query(SQL语句,[是否返回结果]);
+      		//调用方式 query(SQL语句,[是否返回结果]);
 		function query($query,$res = 1){
 			$connect = mysqli_connect($this->location,$this->usr,$this->pwd);
 			mysqli_select_db($connect,$this->db);
 			if($result = mysqli_query($connect,$query)){
-              	if($res){
-            	    $tag = 0;
-                    while($row = mysqli_fetch_row($result)){
-                        $return[$tag] = $row[0];
-                        $tag++;
-                    }
-                    return $return;
-                }
-              	else{
-                    return 1;
-                }
-            }
+				if($res){
+			            $tag = 0;
+				    while($row = mysqli_fetch_row($result)){
+					$return[$tag] = $row[0];
+					$tag++;
+				    }
+   				return $return;
+                		}
+			}
+              		else{
+                		return 1;
+                	}
 		}
 	}
 ?>
