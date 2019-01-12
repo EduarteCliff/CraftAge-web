c7f64cd8862e280aa9cfb71ff879b66262f00359
EduarteCliff <37478960+EduarteCliff@users.noreply.github.com>
2019-01-12 23:09:03 +0800

    bug fix

--- class/class.sql.php
+++ class/class.sql.php
@@ -1,25 +1,26 @@
 <?php
-	public class sql{
+	class sql{
 		public $location = "127.0.0.1";
 		public $usr;
 		public $pwd;
 		public $db;
-		
-		function query($query){
+      	
+		function query($query,$res = 1){
 			$connect = mysqli_connect($this->location,$this->usr,$this->pwd);
 			mysqli_select_db($connect,$this->db);
-			$result = mysqli_query($connect,$query);
-			mysql_close($connect);
-			return decode($result);
-		}
-		
-		function decode($result){
-			$tag = 0;
-			while($row = mysqli_fetch_row($result)){
-				$return[$tag] = $row[0];
-				$tag++;
-			}
-			return $return;
+			if($result = mysqli_query($connect,$query)){
+              	if($res){
+            	$tag = 0;
+                    while($row = mysqli_fetch_row($result)){
+                        $return[$tag] = $row[0];
+                        $tag++;
+                    }
+                  	return $return;
+                }
+              	else{
+                	return 1;
+                }
+            }
 		}
 	}
 ?>
