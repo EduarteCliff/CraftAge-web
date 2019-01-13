ba71f9dca66eba80e66af160c68e427cd917ef45
EduarteCliff <37478960+EduarteCliff@users.noreply.github.com>
2019-01-13 10:13:07 +0800

    Update class.sql.php

--- class/class.sql.php
+++ class/class.sql.php
@@ -1,27 +1,33 @@
 <?php
-class sql{
-	public $location = "127.0.0.1";
-	public $usr;
-	public $pwd;
-	public $db;
+	class sql{
+		public $location = "127.0.0.1";
+		public $usr;
+		public $pwd;
+		public $db;
 		
-      	//调用方式 query(SQL语句,[是否返回结果]);
-	function query($query,$res = 1){
-		$connect = mysqli_connect($this->location,$this->usr,$this->pwd);
-		mysqli_select_db($connect,$this->db);
-		if($result = mysqli_query($connect,$query)){
-			if($res){
-				$tag = 0;
-				while($row = mysqli_fetch_row($result)){
-					$return[$tag] = $row[0];
-					$tag++;
-				}
-   				return $return;
-                	}
+		//建立连接
+		function connect(){
+			$connect = mysqli_connect($this->location,$this->usr,$this->pwd);
+			mysqli_select_db($connect,$this->db);
+			return $connect;
 		}
-              	else{
-                	return 1;
-                }
+
+		//调用方式 query(SQL语句,[是否返回结果]);
+		function query($query,$res = 1){
+			$connect = $this->connect();
+			if($result = mysqli_query($connect,$query)){
+				if($res){
+					$tag = 0;
+					while($row = mysqli_fetch_row($result)){
+						$return[$tag] = $row[0];
+						$tag++;
+					}
+					return $return;
+				}
+				else{
+					return 1;
+				}
+			}
+		}
 	}
-}
 ?>
